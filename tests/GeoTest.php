b74c5ad8322f8cadfe67d2a914875124afa0c825
Alexander Strizhak <gam6itko@gmail.com>
2024-05-14 11:45:53 +0300

    github workflow (#3)
    add github workflow

--- tests/GeoTest.php
+++ tests/GeoTest.php
@@ -7,11 +7,15 @@ namespace Sypex\Tests;
 use PHPUnit\Framework\TestCase;
 use Sypex\Geo;
 
-class GeoTest extends TestCase
+/**
+ * @coversDefaultClass \Sypex\Geo
+ */
+final class GeoTest extends TestCase
 {
     private const FILE_PATH = __DIR__.'/Resources/SxGeo.dat';
 
     /**
+     * @covers ::getCountry
      * @dataProvider dataGetCountry
      */
     public function testGetCountry(string $ip, string $expected): void
@@ -20,9 +24,36 @@ class GeoTest extends TestCase
         self::assertSame($expected, $geo->getCountry($ip));
     }
 
-    public function dataGetCountry(): iterable
+    public static function dataGetCountry(): iterable
     {
         yield ['8.8.8.8', 'US'];
         yield ['95.173.136.72', 'RU'];
+    }
+
+    /**
+     * @covers ::getCity
+     * @dataProvider dataGetCity
+     */
+    public function testGetCity(string $ip, $expected): void
+    {
+        $geo = new Geo(self::FILE_PATH);
+        self::assertSame($expected, $geo->getCity($ip));
+    }
+
+    public static function dataGetCity(): iterable
+    {
+        yield ['8.8.8.8', false];
+    }
+
+    /**
+     * @covers ::about
+     */
+    public function testAbout(): void
+    {
+        $geo = new Geo(self::FILE_PATH);
+        $about = $geo->about();
+        self::assertArrayHasKey('Created', $about);
+        self::assertArrayHasKey('Charset', $about);
+        self::assertArrayHasKey('Type', $about);
     }
 }
